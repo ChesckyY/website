38100d86183c792e316f390331e8aded1e3c39f3
ChesckyY <cheskyyusuf57@gmail.com>
2025-12-09 10:35:59 +0700

    yeah

--- src/app.js
+++ src/app.js
@@ -15,6 +15,19 @@ app.get('/health', (req, res) => {
     res.status(200).send('OK');
 });
 
+// gunakan variabel port yang sudah dideklarasi
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+});
+
+// tangani error global agar tercatat di journal/systemd
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err && err.stack ? err.stack : err);
+    // beri waktu agar log flush, lalu exit (systemd bisa restart)
+    setTimeout(() => process.exit(1), 100);
+});
+
+process.on('unhandledRejection', (reason) => {
+    console.error('Unhandled Rejection:', reason);
+    setTimeout(() => process.exit(1), 100);
 });
